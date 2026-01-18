14fb8107b1135b19283ea76710d8dfed4899c3f7
Peter McGowan <peteburquette@gmail.com>
2026-01-18 00:55:35 +0000

    static build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 1. This tells Next.js to create a static 'out' folder
+  output: 'export',
+
+  // 2. REQUIRED for Static Export: Disable server-side image optimization
+  // (Because S3 cannot optimize images on the fly like a Node server)
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
